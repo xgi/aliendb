ac031e866213eadcb5fb0a80bc647863bc6e0257
Jake Robertson <jake@faltro.com>
2017-06-16 02:50:41 -0400

    fix how adding fake subdirectory to url would still function

--- web/aliendb/apps/analytics/urls.py
+++ web/aliendb/apps/analytics/urls.py
@@ -6,10 +6,10 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
-    url(r'^subreddits', views.subreddits, name='subreddits'),
-    url(r'^about', views.about, name='about'),
-    url(r'^api', views.api, name='api'),
-    url(r'^submission/(?P<id>[\w]+)', views.submission, name='submission'),
-    url(r'^subreddit/(?P<subreddit>[\w]+)', views.subreddit, name='subreddit'),
-    url(r'^search', views.search, name='search')
+    url(r'^subreddits$', views.subreddits, name='subreddits'),
+    url(r'^about$', views.about, name='about'),
+    url(r'^api$', views.api, name='api'),
+    url(r'^submission/(?P<id>[\w]+)$', views.submission, name='submission'),
+    url(r'^subreddit/(?P<subreddit>[\w]+)$', views.subreddit, name='subreddit'),
+    url(r'^search$', views.search, name='search')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
