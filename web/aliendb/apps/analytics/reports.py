e490126a1020d52698111ea29a33c3c09c6c00b8
Jake Robertson <jake@faltro.com>
2017-06-16 20:23:47 -0400

    use subreddit model in submission report

--- web/aliendb/apps/analytics/reports.py
+++ web/aliendb/apps/analytics/reports.py
@@ -18,22 +18,11 @@ def submission(request):
     if data is not None:
         return data
 
+    subreddit = submission.subreddit
     comments = Comment.objects.filter(submission=submission)
     submission_scores = SubmissionScore.objects.filter(submission=submission).order_by('timestamp')
     submission_num_comments = SubmissionNumComments.objects.filter(submission=submission).order_by('timestamp')
     submission_upvote_ratios = SubmissionUpvoteRatio.objects.filter(submission=submission).order_by('timestamp')
-
-    # try to get some variables from cache
-    subreddit_submissions = cache.get("subreddit_submissions_%s" % submission.subreddit)
-    subreddit_comments = cache.get("subreddit_comments_%s" % submission.subreddit)
-
-    # check if they weren't found in cache
-    if subreddit_submissions is None:
-        subreddit_submissions = Submission.objects.filter(subreddit=submission.subreddit)
-        cache.set("subreddit_submissions_%s" % submission.subreddit, subreddit_submissions, 600)
-    if subreddit_comments is None:
-        subreddit_comments = [c for queryset in [Comment.objects.filter(submission_id=s.id) for s in subreddit_submissions] for c in queryset]
-        cache.set("subreddit_comments_%s" % submission.subreddit, subreddit_comments, 600)
 
     ## activity
     score_tallies = [[int((s.timestamp - epoch).total_seconds()) * 1000.0, s.score] for s in submission_scores]
@@ -41,13 +30,6 @@ def submission(request):
 
     ## upvote_ratio
     upvote_ratios = [[int((s.timestamp - epoch).total_seconds()) * 1000.0, s.upvote_ratio] for s in submission_upvote_ratios]
-
-    # get average ratio for subreddit
-    total_upvote_ratio = 0
-    for submission in subreddit_submissions:
-        total_upvote_ratio += submission.upvote_ratio
-    average_upvote_ratio = total_upvote_ratio / len(subreddit_submissions)
-    average_upvote_ratio = float("{0:.2f}".format(average_upvote_ratio))
 
     ## special_users
     special_users_submission = [
@@ -57,15 +39,14 @@ def submission(request):
         [c.is_special for c in comments].count(True)
     ]
     special_users_subreddit = [
-        [c.is_op for c in comments].count(True) / len(subreddit_submissions),
-        [c.is_mod for c in comments].count(True) / len(subreddit_submissions),
-        [c.is_admin for c in comments].count(True) / len(subreddit_submissions),
-        [c.is_special for c in comments].count(True) / len(subreddit_submissions)
+        subreddit.average_is_op,
+        subreddit.average_is_mod,
+        subreddit.average_is_admin,
+        subreddit.average_is_special,
     ]
 
     ## gilded
     gilded_submission = sum(c.gilded for c in comments)
-    gilded_subreddit = sum(c.gilded for c in subreddit_comments) / len(subreddit_submissions)
 
     ## polarity
     polarity_submission = [
@@ -73,8 +54,8 @@ def submission(request):
         sum(c.polarity for c in comments) / len(comments)
     ]
     polarity_subreddit = [
-        sum(s.polarity for s in subreddit_submissions) / len(subreddit_submissions),
-        sum(c.polarity for c in subreddit_comments) / len(subreddit_comments)
+        subreddit.average_submission_polarity,
+        subreddit.average_comments_polarity
     ]
 
     ## subjectivity
@@ -83,8 +64,8 @@ def submission(request):
         sum(c.subjectivity for c in comments) / len(comments)
     ]
     subjectivity_subreddit = [
-        sum(s.subjectivity for s in subreddit_submissions) / len(subreddit_submissions),
-        sum(c.subjectivity for c in subreddit_comments) / len(subreddit_comments)
+        subreddit.average_submission_subjectivity,
+        subreddit.average_comments_subjectivity
     ]
 
     data = {
@@ -94,7 +75,7 @@ def submission(request):
         },
         'upvote_ratio': {
             'upvote_ratios': upvote_ratios,
-            'average_upvote_ratio': average_upvote_ratio
+            'average_upvote_ratio': subreddit.average_upvote_ratio
         },
         'special_users': {
             'submission': special_users_submission,
@@ -103,7 +84,7 @@ def submission(request):
         'gilded': {
             'data': [
                 gilded_submission,
-                gilded_subreddit
+                subreddit.average_gilded
             ]
         },
         'polarity': {
