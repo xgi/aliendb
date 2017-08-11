4883296f11e20895357e7e7f6c299f9fd2114d67
Jake Robertson <jake@faltro.com>
2017-08-10 23:06:42 -0400

    dont automatically make migrations

--- web/startup.sh
+++ web/startup.sh
@@ -6,15 +6,6 @@ do
   sleep 0.5
 done
 
-# find all apps and make migrations
-for dir in $(find aliendb/apps -maxdepth 1 -type d \
-             ! -name __pycache__ \
-             ! -name apps)
-do
-  app_name=$(basename $dir)
-  python manage.py makemigrations $app_name
-done
-
 # apply migrations
 python manage.py migrate
 
