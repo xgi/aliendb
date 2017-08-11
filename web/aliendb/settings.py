e765c7b07bd169d5fd0b47008a8ec757a35dfc05
Jake Robertson <jake@faltro.com>
2017-08-11 15:53:19 -0400

    move debug env var to .env file

--- web/aliendb/settings.py
+++ web/aliendb/settings.py
@@ -20,7 +20,7 @@ SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True if os.getenv('DEBUG') == 'true' else False
+DEBUG = True if os.environ['DEBUG'].lower() == 'true' else False
 
 ALLOWED_HOSTS = ['*']
 
