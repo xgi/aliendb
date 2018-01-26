9bd5b66a50def87de2b8a37ba452ee4efc8a17b7
Jake Robertson <jake@faltro.com>
2018-01-26 17:09:57 -0500

    add docstring for update_average

--- web/aliendb/apps/analytics/helpers.py
+++ web/aliendb/apps/analytics/helpers.py
@@ -1,3 +1,12 @@
-def update_average(field, value, tracked):
+def update_average(field, value, tracked) -> float:
+    """Updates a previously calculated average with a new value.
+
+    Args:
+        field: the current average;
+        value: the new value to include in the average;
+        tracked: the number of elements used to form the _original_ average;
+
+    Returns:
+        float: the updated average
+    """
     return (value + field * tracked) / (1 + tracked)
-    
