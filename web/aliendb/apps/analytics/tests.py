5cb9f7d37be6173a38fdc5f03ff7dd067b527ff5
Jake Robertson <jake@faltro.com>
2018-02-14 18:29:49 -0500

    create some basic api tests

--- web/aliendb/apps/analytics/tests.py
+++ web/aliendb/apps/analytics/tests.py
@@ -71,12 +71,6 @@ class ViewsTest(TestCase):
     def test_about_200(self):
         response = self.client.get('/about')
         self.assertEqual(response.status_code, 200)
-    def test_api_200(self):
-        response = self.client.get('/api?name=submission&id=000001')
-        self.assertEqual(response.status_code, 200)
-    def test_api_404(self):
-        response = self.client.get('/api?name=submission&id=222222')
-        self.assertEqual(response.status_code, 404)
     def test_submission_200(self):
         response = self.client.get('/submission/000001')
         self.assertEqual(response.status_code, 200)
@@ -98,6 +92,29 @@ class ViewsTest(TestCase):
     def test_search_match(self):
         response = self.client.get('/search?q=mytitle&order_by=karma&time=all&subreddits=testsubreddit')
         self.assertContains(response, "Found 1 match")
+
+class APITest(TestCase):
+    # TODO: Add more methods to test returned API values. Will likely require a
+    # more elaborate data set to test from. Maybe freeze a copy of the database
+    # to use for this kind of testing?
+    def setUp(self):
+        self.client = Client()
+        create_dummy_models()
+    def test_api_submission_200(self):
+        response = self.client.get('/api?name=submission&id=000001')
+        self.assertEqual(response.status_code, 200)
+    def test_api_submission_404(self):
+        response = self.client.get('/api?name=submission&id=222222')
+        self.assertEqual(response.status_code, 404)
+    def test_api_subreddit_200(self):
+        response = self.client.get('/api?name=subreddit&id=testsubreddit')
+        self.assertEqual(response.status_code, 200)
+    def test_api_subreddit_404(self):
+        response = self.client.get('/api?name=subreddit&id=notarealsubreddit')
+        self.assertEqual(response.status_code, 404)
+    def test_api_cumulative_200(self):
+        response = self.client.get('/api?name=cumulative&timerange=day')
+        self.assertEqual(response.status_code, 200)
 
 @override_settings(DEBUG=False)
 class CacheTest(TestCase):
