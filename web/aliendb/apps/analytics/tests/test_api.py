144ebe83632d0813412465468ee711ddf13549ad
Jake Robertson <jake@faltro.com>
2018-04-21 19:50:29 -0400

    minor api test changes to improve coverage

--- web/aliendb/apps/analytics/tests/test_api.py
+++ web/aliendb/apps/analytics/tests/test_api.py
@@ -12,21 +12,31 @@ class APITest(TestCase):
         create_dummy_models()
 
     def test_api_submission_200(self):
-        response = self.client.get('/api?name=submission&id=000001')
-        self.assertEqual(response.status_code, 200)
+        # run twice to hit the cache
+        response1 = self.client.get('/api?name=submission&id=000001')
+        response2 = self.client.get('/api?name=submission&id=000001')
+        assert response1.content == response2.content
+        self.assertEqual(response1.status_code, 200)
 
     def test_api_submission_404(self):
         response = self.client.get('/api?name=submission&id=222222')
         self.assertEqual(response.status_code, 404)
 
     def test_api_subreddit_200(self):
-        response = self.client.get('/api?name=subreddit&id=testsubreddit')
-        self.assertEqual(response.status_code, 200)
+        # run twice to hit the cache
+        response1 = self.client.get('/api?name=subreddit&id=testsubreddit')
+        response2 = self.client.get('/api?name=subreddit&id=testsubreddit')
+        assert response1.content == response2.content
+        self.assertEqual(response1.status_code, 200)
 
     def test_api_subreddit_404(self):
         response = self.client.get('/api?name=subreddit&id=notarealsubreddit')
         self.assertEqual(response.status_code, 404)
 
     def test_api_cumulative_200(self):
-        response = self.client.get('/api?name=cumulative&timerange=day')
-        self.assertEqual(response.status_code, 200)
+        timeranges = ['day', 'week', 'fortnight', 'month', 'year']
+        for timerange in timeranges:
+            response = self.client.get(
+                '/api?name=cumulative&timerange=%s' % timerange
+            )
+            self.assertEqual(response.status_code, 200)
