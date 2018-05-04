31baaa9da2aec16ba5ea97ad6ba124023287aa36
Jake Robertson <jake@faltro.com>
2018-05-03 20:21:24 -0400

    omit names from url functions

--- web/aliendb/apps/analytics/urls.py
+++ web/aliendb/apps/analytics/urls.py
@@ -5,11 +5,11 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
-    url(r'^subreddits$', views.subreddits, name='subreddits'),
-    url(r'^about$', views.about, name='about'),
-    url(r'^api$', views.api, name='api'),
-    url(r'^submission/(?P<id>[\w]+)$', views.submission, name='submission'),
-    url(r'^subreddit/(?P<subreddit>[\w]+)$', views.subreddit, name='subreddit'),
-    url(r'^search$', views.search, name='search')
+    url(r'^$', views.home),
+    url(r'^subreddits$', views.subreddits),
+    url(r'^about$', views.about),
+    url(r'^api$', views.api),
+    url(r'^submission/(?P<id>[\w]+)$', views.submission),
+    url(r'^subreddit/(?P<subreddit>[\w]+)$', views.subreddit),
+    url(r'^search$', views.search)
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
