7e1f899fdb6eca5b87b4106d8a199d9d6ba74c10
Jake Robertson <jake@faltro.com>
2019-02-15 20:05:17 -0500

    add separate trackers for silver/gold/platinum

--- web/aliendb/apps/analytics/tasks.py
+++ web/aliendb/apps/analytics/tasks.py
@@ -74,6 +74,9 @@ def create_submission_obj(submission, rank) -> Submission:
                                 over_18=submission.over_18,
                                 spoiler=submission.spoiler,
                                 locked=submission.locked,
+                                gilded_silver=submission.gildings['gid_1'],
+                                gilded_gold=submission.gildings['gid_2'],
+                                gilded_platinum=submission.gildings['gid_3'],
                                 created_at=created_at)
     submission_obj.save()
 
@@ -127,6 +130,9 @@ def update_submission_obj(submission, rank) -> Submission:
     submission_obj.over_18 = submission.over_18
     submission_obj.spoiler = submission.spoiler
     submission_obj.locked = submission.locked
+    submission_obj.gilded_silver = submission.gildings['gid_1']
+    submission_obj.gilded_gold = submission.gildings['gid_2']
+    submission_obj.gilded_platinum = submission.gildings['gid_3']
 
     # create new Comment objects if necessary
     if submission.num_comments > 500:
@@ -192,7 +198,9 @@ def create_comment_obj(comment, submission_obj):
                               is_mod=is_mod,
                               is_admin=is_admin,
                               is_special=is_special,
-                              gilded=comment.gilded,
+                              gilded_silver=comment.gildings['gid_1'],
+                              gilded_gold=comment.gildings['gid_2'],
+                              gilded_platinum=comment.gildings['gid_3'],
                               characters=num_characters,
                               words=len(blob.words),
                               sentences=len(blob.sentences),
@@ -218,7 +226,9 @@ def create_comment_obj(comment, submission_obj):
         # comment already exists in db
         comment_obj = Comment.objects.get(id=comment.id)
         comment_obj.score = comment.score
-        comment_obj.gilded = comment.gilded
+        comment_obj.gilded_silver = comment.gildings['gid_1']
+        comment_obj.gilded_gold = comment.gildings['gid_2']
+        comment_obj.gilded_platinum = comment.gildings['gid_3']
         comment_obj.save()
 
 
@@ -234,7 +244,9 @@ def update_subreddit_obj(submission_obj) -> Subreddit:
     subreddit = submission_obj.subreddit
     comments = Comment.objects.filter(submission=submission_obj)
 
-    current_gilded = sum(c.gilded for c in comments)
+    current_gilded_silver = sum(c.gilded_silver for c in comments)
+    current_gilded_gold = sum(c.gilded_gold for c in comments)
+    current_gilded_platinum = sum(c.gilded_platinum for c in comments)
     current_is_op = [c.is_op for c in comments].count(True)
     current_is_mod = [c.is_mod for c in comments].count(True)
     current_is_admin = [c.is_admin for c in comments].count(True)
@@ -252,9 +264,17 @@ def update_subreddit_obj(submission_obj) -> Subreddit:
         subreddit.average_upvote_ratio,
         submission_obj.upvote_ratio,
         subreddit.tracked_submissions)
-    subreddit.average_gilded = update_average(
-        subreddit.average_gilded,
-        current_gilded,
+    subreddit.average_gilded_silver = update_average(
+        subreddit.average_gilded_silver,
+        current_gilded_silver,
+        subreddit.tracked_submissions)
+    subreddit.average_gilded_gold = update_average(
+        subreddit.average_gilded_gold,
+        current_gilded_gold,
+        subreddit.tracked_submissions)
+    subreddit.average_gilded_platinum = update_average(
+        subreddit.average_gilded_platinum,
+        current_gilded_platinum,
         subreddit.tracked_submissions)
     subreddit.average_is_op = update_average(
         subreddit.average_is_op,
